f0c30cdb6bf9b16e1db9292a39fadbcd38ff0620
John Doe <mat@crzy.co.uk>
2013-05-16 21:23:37 +0100

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ progo_helloworld
 ================
 
 Project GO : hello world
+
+So here it is, the first part of project Go!
+
+It's very simple, the usual hello world! program.
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+// load the fmt package to print to stdout
+import "fmt"
+
+// main function
+func main() {
+	// print the line to stdout
+	fmt.Println("Hello World!")
+}
